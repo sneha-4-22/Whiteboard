dbf635003815718488e7402f4ed8ed62c37339a2
sneha kumari <snehakumari64445@gmail.com>
2026-05-03 15:03:57 +0530

    fill color bug fix

--- src/components/Toolbox/index.js
+++ src/components/Toolbox/index.js
@@ -65,7 +65,7 @@ const Toolbox = () => {
                 <input
                   className={classes.colorPicker}
                   type="color"
-                  value={strokeColor}
+                 value={fillColor || "#000000"}
                   onChange={(e) => changeFill(activeToolItem, e.target.value)}
                 ></input>
               </div>
